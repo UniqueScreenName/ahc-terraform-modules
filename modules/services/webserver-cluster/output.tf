8e5270267964f820e8ef9eea5887d429b8743423
nxg126 <neil.graham@cbsa-asfc.gc.ca>
2019-12-16 14:15:53 -0500

    add output of the asg so it can be referenced elsewhere

--- modules/services/webserver-cluster/output.tf
+++ modules/services/webserver-cluster/output.tf
@@ -4,3 +4,8 @@ output "elb_dns_name" {
     value=aws_elb.scalableec2-elb.dns_name
     description="The public DNS name of the ELB"
 }
+
+output "asg_name" {
+    value=aws_autoscaling_group.scalableec2-asg.name
+    description="The name of the autoscaling group"
+}
